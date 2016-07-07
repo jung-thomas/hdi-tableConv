8e098af4b3dd47249592a1948ded0425c5ff0c50
XSA_TECHED <XSA_TECHED@unknown>
2016-07-07 23:01:17 +0200

    Fixes #2

--- js/router/index.js
+++ js/router/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function(app) {
-<<<<<<< HEAD
+
 	app.use("/rest/schemas", require("./routes/schemas").router());
 	app.use("/rest/tables", require("./routes/tables").router());
 	app.use("/rest/table", require("./routes/table").router());
@@ -10,14 +10,5 @@ module.exports = function(app) {
 	app.use("/rest/hdbcds", require("./routes/hdbcds").router());
 	app.use("/rest/download", require("./routes/download").router());
 	app.use("/rest/sessionInfo", require("./routes/sessionInfo").router());
-=======
-	app.use("/node/schemas", require("./routes/schemas").router());
-	app.use("/node/tables", require("./routes/tables").router());
-	app.use("/node/table", require("./routes/table").router());
-	app.use("/node/fields", require("./routes/fields").router());
-	app.use("/node/constraints", require("./routes/constraints").router());
-	app.use("/node/hdbcds", require("./routes/hdbcds").router());
-	app.use("/node/download", require("./routes/download").router());
-	app.use("/node/sessionInfo", require("./routes/sessionInfo").router());
->>>>>>> refs/remotes/origin/master
+
 };
