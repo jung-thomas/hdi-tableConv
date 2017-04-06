7dcf70bb50afde9de5ce3a56287f2848f6059551
WORKSHOP_00 <thomas.jung@sap.com>
2017-04-06 12:49:20 -0400

    Update for HANA 2.0 SPS 01 and SAPUI5 1.44

--- js/utils/initialize.js
+++ js/utils/initialize.js
@@ -2,11 +2,15 @@
 "use strict";
 module.exports = {
 	initExpress: function() {
-		var xsenv = require("sap-xsenv");
+		var xsenv = require("@sap/xsenv");
 		var passport = require("passport");
-		var xssec = require("sap-xssec");
-		var xsHDBConn = require("sap-hdbext");
+		var xssec = require("@sap/xssec");
+		var xsHDBConn = require("@sap/hdbext");
 		var express = require("express");
+
+		//logging
+		var logging = require("@sap/logging");
+		var appContext = logging.createAppContext();
 
 		//Initialize Express App for XS UAA and HDBEXT Middleware
 		var app = express();
@@ -15,6 +19,7 @@ module.exports = {
 				tag: "xsuaa"
 			}
 		}).uaa));
+		app.use(logging.expressMiddleware(appContext));
 		app.use(passport.initialize());
 
 		var options = xsenv.getServices({
